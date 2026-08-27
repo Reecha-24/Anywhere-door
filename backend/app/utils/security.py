b8a0102a5021c9ac92dc0782ea13ae2f038d53ce
Reecha Kumari <reecha242002@gmail.com>
2026-08-27 17:11:24 +0530

    pr29

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -1,6 +1,6 @@
 import hashlib
 from passlib.context import CryptContext
-# password : very_secure_123_
+# password : very_secure_123
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
