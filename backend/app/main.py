a6af8d9bf5066e41c69e6259462554a0dcd3ece1
Reecha Kumari <reecha242002@gmail.com>
2026-08-27 15:46:43 +0530

    pr21

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ from app.storage.s3 import s3_manager
 from app.routers import shares, files
 # diff --git a/{filename} b/{filename}\n--- a/{filename}\n+++ b/{filename}\n
 # Not so important chhange 891012
-# secret =7865t5667
+# secret =7865t5667t6
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Initialize AWS Services (DynamoDB & S3)
